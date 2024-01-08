793223f6b1aaf423dca4af1d91e65857e5490da0
Dev Vikram <78585046+techtrigon@users.noreply.github.com>
2024-01-08 23:26:30 +0530

    modified

--- prototypal_inheritence.js
+++ prototypal_inheritence.js
@@ -35,3 +35,44 @@ rect.move (1, 1); // Logs 'Shape moved.'
 rect.move (1, 3); // Logs 'Shape moved.'
 console.log (rect);
 console.log (rect.constructor);
+
+// Object creation without using new keyword and returning object ---------------------------------
+function createuser (name, age) {
+  const user = Object.create (createuser.__proto__);
+  user.name = name;
+  user.age = age;
+  return user;
+}
+createuser.__proto__.about = function () {
+  console.log (`this.name = ${this.name} and this.age = ${this.age}`);
+};
+
+const u1 = createuser ('Dev', 23);
+const u2 = createuser ('Sam', 12);
+
+// Check if the prototypes are the same
+console.log (Object.getPrototypeOf (u1) === Object.getPrototypeOf (u2)); // Output: true
+
+console.log (u1.about === u2.about);
+u1.about ();
+u1.__proto__.x = 2;
+console.log (
+  `u1 prototype is = ${u1.__proto__} and createuser prototype is = ${createuser.__proto__}`
+);
+console.log (createuser.__proto__);
+
+// Object creation using new keyword -------------------------------------
+
+//       Constuctor function below
+function createuser1 (name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+createuser1.prototype.score = function (sc) {
+  console.log (`${this.name} is of age ${this.age} and score is ${sc} `);
+};
+
+const a1 = new createuser1 ('Dev', 23);
+a1.score (24);
+console.log (a1);
